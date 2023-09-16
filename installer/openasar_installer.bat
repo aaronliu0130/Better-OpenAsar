0b6d78ec3eef364bafdf4a8c56ca2586df3d737e
Aaron Liu <aaronliu0130@gmail.com>
2023-09-16 13:28:11 -0400

    Update openasar_installer.bat

--- installer/openasar_installer.bat
+++ installer/openasar_installer.bat
@@ -6,29 +6,7 @@ setlocal enabledelayedexpansion
 rem lil animation, can be skipped by pressing any key 3 times
 cls
 echo.
-echo Installer updated by @aaronliu and maintained by @greenman36
-echo.
-echo ####  ###  #### #   #      ##  ####  ##  ###  
-echo #  #  #  # #    ##  #     #  # #    #  # #  # 
-echo #  #  ###  #### # # #     #### #### #### ###  
-echo #  #  #    #    #  ##     #  #    # #  # #  # 
-echo ####  #    #### #   #  #  #  # #### #  # #  # 
-echo.
-C:\Windows\System32\TIMEOUT.exe /t 1 > nul 2> nul
-cls
-echo.
-echo Installer updated by @aaronliu and maintained by @greenman36
-echo.
-echo ====  ===  ==== =   =      ==  ====  ==  ===  
-echo =  =  =  = =    ==  =     =  = =    =  = =  = 
-echo =  =  ===  ==== = = =     ==== ==== ==== ===  
-echo =  =  =    =    =  ==     =  =    = =  = =  = 
-echo ====  =    ==== =   =  =  =  = ==== =  = =  = 
-echo.
-C:\Windows\System32\TIMEOUT.exe /t 1 > nul 2> nul
-cls
-echo.
-echo Installer updated by @aaronliu and maintained by @greenman36
+echo Installer created by @greenman36 and updated by @aaronliu
 echo.
 echo ....  ...  .... .   .      ..  ....  ..  ...  
 echo .  .  .  . .    ..  .     .  . .    .  . .  . 
@@ -36,15 +14,39 @@ echo .  .  ...  .... . . .     .... .... .... ...
 echo .  .  .    .    .  ..     .  .    . .  . .  . 
 echo ....  .    .... .   .  .  .  . .... .  . .  . 
 echo.
-C:\Windows\System32\TIMEOUT.exe /t 1 > nul 2> nul
+timeout /t 1 >nul
+cls
+echo.
+echo Installer created by @greenman36 and updated by @aaronliu
+echo.
+echo ====  ===  ==== =   =      ==  ====  ==  ===  
+echo =  =  =  = =    ==  =     =  = =    =  = =  = 
+echo =  =  ===  ==== = = =     ==== ==== ==== ===  
+echo =  =  =    =    =  ==     =  =    = =  = =  = 
+echo ====  =    ==== =   =  =  =  = ==== =  = =  = 
+echo.
+timeout /t 1 >nul
+cls
+echo.
+echo Installer created by @greenman36 and updated by @aaronliu
+echo.
+echo ####  ###  #### #   #      ##  ####  ##  ###  
+echo #  #  #  # #    ##  #     #  # #    #  # #  # 
+echo #  #  ###  #### # # #     #### #### #### ###  
+echo #  #  #    #    #  ##     #  #    # #  # #  # 
+echo ####  #    #### #   #  #  #  # #### #  # #  # 
+echo.
+timeout /t 1 >nul
 cls
 
+:select
 rem Discord flavor selection menu
 echo.
-echo Installer updated by @aaronliu and maintained by @greenman36
+echo Installer created by @greenman36 and updated by @aaronliu
 echo.
-echo Select Discord version:
-echo 1. Discord Stable (Default Client)
+echo Select the Discord channel to install to.
+echo.
+echo 1. Discord Stable (Default)
 echo 2. Discord PTB
 echo 3. Discord Canary
 echo.
@@ -58,20 +60,25 @@ if "%selection%"=="1" (
 ) else if "%selection%"=="3" (
     set "discordApp=DiscordCanary"
 ) else if "%selection%"=="" (
-    echo No input detected. Defaulting to Discord Stable.
+    color f6
+    echo Warning: No input detected. Defaulting to Discord Stable.
+    echo.
+    pause
+    color 07
     set "discordApp=Discord"
 ) else (
-	color F4
-    echo Invalid selection. Please try again.
-    color
+	color f4
+    echo Invalid selection. Please only enter a number from one to three.
+    echo.
     pause
-    exit /b
+    color 07
+    cls
+    goto select
 )
 
 
 rem Finds the latest version folder for the selected Discord flavor
 set "latestVersion="
-
 for /f "delims=" %%d in ('dir /b /ad /on "%localappdata%\%discordApp%\app-*"') do (
     set "folderName=%%~nxd"
     rem just the version number (without the app- prefix)
@@ -80,104 +87,89 @@ for /f "delims=" %%d in ('dir /b /ad /on "%localappdata%\%discordApp%\app-*"') d
         set "latestVersion=!version!"
     )
 )
-
 rem If no version folders are found, exit. We can't continue
 if not defined latestVersion (
-    color F4
-    echo No version folders found.
-    color
-    pause
-    exit /b
+    set "error=No version folders found. Do you even have %discordApp% installed?"
+    goto error
 )
 
-echo Closing Discord... (wait around 3 seconds)
+echo Closing Discord...
 echo.
-
-rem Kills Discord multiple times to make sure it's closed
-for /l %%i in (1,1,3) do (
-    C:\Windows\System32\TASKKILL.exe /f /im %discordApp%.exe > nul 2> nul
-)
-
-rem Waits 3 seconds to make sure Discord is fully closed
-C:\Windows\System32\TIMEOUT.exe /t 3 /nobreak > nul 2> nul
+taskkill /f /im %discordApp%.exe
 cls
 
 rem Let the user make sure all info is correct before continuing
-echo.
-echo Installer updated by @aaronliu and maintained by @greenman36
+echo Installer created by @greenman36 and updated by @aaronliu
 echo.
 echo Confirm the following information before continuing.
 echo.
-echo Version: %discordApp%
-echo App version: %latestVersion%
-echo Full path: %localappdata%\%discordApp%\app-%latestVersion%\resources\
+echo Channel: %discordApp%
+echo Host Version: %latestVersion%
+echo Full Path: %localappdata%\%discordApp%\app-%latestVersion%\resources\
 echo.
 pause
 
-echo Installing OpenAsar... (ignore any flashes, this is a download progress bar)
+echo Installing OpenAsar...
 echo.
 echo 1. Backing up original app.asar to app.asar.backup
+if exist "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.backup" (
+    color f6
+    echo Warning: A backup already exists. OpenAsar can auto-update and there's no need to reinstall.
+    echo Are you sure you still want to continue?
+    echo.
+    pause
+    color 07
+)
 rem This is done multiple times because there's multiple client mods that use different file names and we support those
-copy /y "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar" "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.backup" > nul 2> nul
+move /y "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar" "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.backup" >nul
 if exist "%localappdata%\%discordApp%\app-%latestVersion%\resources\_app.asar" (
-    copy /y "%localappdata%\%discordApp%\app-%latestVersion%\resources\_app.asar" "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.backup" > nul 2> nul
+    move /y "%localappdata%\%discordApp%\app-%latestVersion%\resources\_app.asar" "%localappdata%\%discordApp%\app-%latestVersion%\resources\_app.asar.backup" >nul
 )
 if exist "%localappdata%\%discordApp%\app-%version%\resources\app.asar.orig" (
-    copy /y "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.orig" "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.backup" > nul 2> nul
+    move /y "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.orig" "%localappdata%\%discordApp%\app-%latestVersion%\resources\app.asar.orig.backup" >nul
 )
-
 rem If the copy command failed, exit
 if errorlevel 1 (
-    color F4
-    echo Error: Failed to copy the file.
-    echo Please check the file paths and try again.
-    echo.
-    color
-    pause
-    exit
+    set "error=Failed to backup the existing ASARs."
+    goto error
 )
 
-echo 2. Downloading OpenAsar
-powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile "%localappdata%\%discordApp%\app-%version%\resources\app.asar"" > nul 2> nul
+echo 2. Downloading OpenAsar (ignore any flashes, this is a download progress bar)
+powershell -Command "Invoke-WebRequest https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar -OutFile "%localappdata%\%discordApp%\app-%version%\resources\app.asar"" >nul
 
-if exist "%localappdata%\%discordApp%\app-%version%\resources\_app.asar" (
+if exist "%localappdata%\%discordApp%\app-%version%\resources\_app.asar.backup" (
     copy "%localappdata%\%discordApp%\app-%version%\resources\app.asar" "%localappdata%\%discordApp%\app-%version%\resources\_app.asar"
 )
-if exist "%localappdata%\%discordApp%\app-%version%\resources\app.asar.orig" (
+if exist "%localappdata%\%discordApp%\app-%version%\resources\app.asar.orig.backup" (
     copy "%localappdata%\%discordApp%\app-%version%\resources\app.asar" "%localappdata%\%discordApp%\app-%version%\resources\app.asar.orig"
 )
 
 rem If the download command failed, exit
 if errorlevel 1 (
-    color F4
-    echo Error: Failed to download and replace the asar file.
-    echo Please check your internet connection. Also make sure that the Discord client is closed.
-    echo.
-    color
-    pause
-    exit
+    set "error=Failed to download and replace the ASARs.\nPlease check your internet connectin and make sure Discord is closed and not in your tray menu."
+    goto error
 )
+
+echo Opening Discord...
+start "" "%localappdata%\%discordApp%\Update.exe" --processStart %discordApp%.exe >nul
 
 rem Change the color to indicate success and start Discord
 cls
-color F2
+color f2
+echo Installer created by @greenman36 and updated by @aaronliu
 echo.
-echo Opening Discord...
-start "" "%localappdata%\%discordApp%\Update.exe" --processStart %discordApp%.exe > nul 2> nul
-
-C:\Windows\System32\TIMEOUT.exe /t 1 /nobreak > nul 2> nul
-
-echo.
-echo.
-echo OpenAsar should be installed! You can check by looking for an "OpenAsar" option in your Discord settings.
+echo OpenAsar should be installed^^! You can check by looking for an "OpenAsar" option in your Discord settings.
 echo Not installed? Try restarting Discord, running the script again, joining the OpenAsar Discord or contacting @greenman36 on Discord.
-echo.
-echo Installer updated by @aaronliu and maintained by @greenman36
-echo Also check out some of my other projects at [GreenMan36.github.io](https://GreenMan36.github.io)
-echo.
-
+echo Also, check out some of our other projects at https://greenman36.github.io and https://github.com/aaronliu0130 and petition Ducko to use this as the official installer^^!
 echo.
 pause
-color
-
+color 07
 exit /b
+
+:error
+color f4
+echo Error: %error%
+echo.
+pause
+color 07
+exit /b 1
