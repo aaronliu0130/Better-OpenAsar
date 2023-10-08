56f330c9021dec2c2add4d55a042fc8f73a5e8d3
aliu <aaronliu0130@gmail.com>
2023-10-08 10:04:00 -0400

    Reorganization
    originally commit 810452d
    treat this as all the inital changes i made in the fork
    trying to reduce repo size

---
+++ README.md
@@ -0,0 +1,46 @@
+# Better OpenAsar Installer
+
+This has a Linux install script and GreenMan36's improved version of the Windows install script.
+
+## Windows
+
+Sometimes, Discord updates its host. This means you'll have to install OpenAsar again, for the new host version. However, the path to the Discord files changes with every host update, and OpenAsar's official installer hardcodes the path to a specific host version. This means that every time Discord updates its host, OpenAsar and you have to update their installer.
+
+GreenMan36's improved version automatically detects the folder for the newest version and installs OpenAsar there.
+
+### Demo
+
+
+
+#### TL;DW:
+
+1. Show a little animation
+2. Pick the Discord version
+3. Confirm the info
+4. Install
+5. Done!
+
+* Installs no matter Discord's version
+* Intuitive error messages
+* Nice li'l animation
+If there's any issues, the installer will give you a nice error message which an be used to quickly debug where things went wrong.
+
+## Linux
+
+On Linux, no updates have changed the path of Discord's files yet. However, there are a ton of paths that could have them, as Discord does not have official packages for every distro (variety) of Linux, and even the official packages might get installed to different places by package managers.
+
+### Bash
+
+* Bash is the most widely installed shell that allows for Bash scripting
+* Try every known location of Discord's files
+* Even better animation
+* Always only download OpenAsar once
+* Automatically try to sudo if installation fails
+* Intuitive input hints
+* Allow specifying a custom path
+
+## Misc
+
+- Ducko doesn't want to use the Windows script
+- Please also check out  <https://github.com/stefanhaustein/TerminalImageViewer> and <https://github.com/upscayl/upscayl>
+- Check out GreenMan36's OpenAsar supported Discord themes at  [![Discord](https://img.shields.io/discord/1050062854860046417?color=7289da&logo=discord&logoColor=white)](https://discord.gg/A6vwGchJYs)
---
+++ installer/openasar-install.bash
@@ -0,0 +1,220 @@
+#!/usr/bin/env bash
+# v1.0: Arong Aarony
+if [[ -z $spaceballs ]]; then
+    # lil animation, can be skipped by exporting spaceballs
+    clear
+    echo
+    echo 'Installer updated and maintained by aaronliu0130 (@aaronliu)'
+    echo
+    echo '....  ...  .... .   .      ..  ....  ..  ...'
+    echo '.  .  .  . .    ..  .     .  . .    .  . .  .'
+    echo '.  .  ...  .... . . .     .... .... .... ...'
+    echo '.  .  .    .    .  ..     .  .    . .  . .  .'
+    echo '....  .    .... .   .  .  .  . .... .  . .  .'
+    echo
+    sleep 1
+    clear
+    echo
+    echo 'Installer updated and maintained by aaronliu0130 (@aaronliu)'
+    echo
+    echo '====  ===  ==== =   =      ==  ====  ==  ==='
+    echo '=  =  =  = =    ==  =     =  = =    =  = =  ='
+    echo '=  =  ===  ==== = = =     ==== ==== ==== ==='
+    echo '=  =  =    =    =  ==     =  =    = =  = =  ='
+    echo '====  =    ==== =   =  =  =  = ==== =  = =  ='
+    echo
+    sleep 1
+    if [[ $(curl -sL 'https://github.com/aaronliu0130/Better-OpenAsar/raw/main/installer/openasar-installer.bash' | sed '2!d') != "# v1.0: Arong Aarony" ]]; then
+        curl -sLo "./openasar-install.bash" 'https://github.com/aaronliu0130/Better-OpenAsar/raw/main/installer/openasar-installer.bash'
+        if ! ./openasar-install.bash; then
+            exit $?
+        else
+            if [ $? -ne 0 ]; then exit $?; fi
+            export spaceballs="If you can read this, you don't need glasses."
+            clear
+            echo
+            echo 'Installer updated and maintained by aaronliu0130 (@aaronliu)'
+            echo
+            echo '====  ===  ==== =   =      ==  ====  ==  ==='
+            echo '=  =  =  = =    ==  =     =  = =    =  = =  ='
+            echo '=  =  ===  ==== = = =     ==== ==== ==== ==='
+            echo '=  =  =    =    =  ==     =  =    = =  = =  ='
+            echo '====  =    ==== =   =  =  =  = ==== =  = =  ='
+            echo
+            echo 'OpenAsar should be installed! You can check by looking for an "OpenAsar" option in your Discord settings.'
+            echo 'Not installed? Try restarting Discord, running this script again, filing a GitHub issue or contacting @aaronliu on Discord.'
+            exit 0
+        fi
+    fi
+fi
+clear
+echo
+echo 'Installer updated and maintained by aaronliu0130 (@aaronliu)'
+echo
+echo '####  ###  #### #   #      ##  ####  ##  ###'
+echo '#  #  #  # #    ##  #     #  # #    #  # #  #'
+echo '#  #  ###  #### # # #     #### #### #### ###'
+echo '#  #  #    #    #  ##     #  #    # #  # #  #'
+echo '####  #    #### #   #  #  #  # #### #  # #  #'
+echo
+echo '1. Discord Stable (default)'
+echo '2. Discord PTB'
+echo '3. Discord Canary'
+echo
+echo Select your Discord channel by number or manually specify the path to app.asar.
+read -r ans
+
+while [[ -z $kebab ]]; do
+    case $ans in
+    1) kebab='discord' ;;
+    2) kebab='discord-ptb' ;;
+    3) kebab='discord-canary' ;;
+    *)
+        if [[ -d "$ans" ]]; then
+            echo Please enter the path to the file, not a directory.
+            read -r ans
+        elif [[ -e "$ans" ]]; then
+            file="$ans"
+        else
+            echo Please enter a number from 1 to 3 corresponding to a version above, or the path to app.asar.
+            read -r ans
+        fi
+        ;;
+    esac
+done
+
+camel=('discord' 'discordPtb' 'discordCanary')
+pascal=('Discord' 'DiscordPtb' 'DiscordCanary')
+plain=('Discord Stable' 'Discord PTB' 'Discord Canary')
+files=(
+    "/opt/$kebab/resources/app.asar"
+    "/var/lib/flatpak/app/com.discordapp.${pascal[ans]}/current/active/files/${camel[ans]}/resources/app.asar"
+    "$HOME/local/share/flatpak/app/com.discordapp.${pascal[ans]}/current/active/files/${camel[ans]}/resources/app.asar"
+    "/usr/lib/$kebab/resources/app.asar"
+    "/usr/lib64/$kebab/resources/app.asar"
+    "/usr/share/$kebab/resources/app.asar"
+    "/usr/lib/$kebab/app.asar"
+    'WtoI-7DWs4o')
+echo Looking for app.asar...
+for file in "${files[@]}"; do
+    if [[ -e "$file" ]]; then
+        break
+    fi
+done
+if [[ "$file" == "WtoI-7DWs4o" ]]; then
+    if [[ $ans == 2 && -e '/usr/lib/discord-development/app.asar' ]]; then
+        file=/usr/lib/discord-development/app.asar
+    else
+        echo "Couldn't find ${plain[ans]}'s app.asar, please specify the path to the app.asar file or input 0 to restart."
+        read -r file
+        if [[ $file == 0 ]]; then
+            export spaceballs=$((spaceballs + 1))
+            if ! ./openasar-install.bash; then
+                exit $?
+            else
+                if [ $? -ne 0 ]; then exit $?; fi
+                sleep 1
+                clear
+                echo
+                echo 'Installer updated and maintained by aaronliu0130 (@aaronliu)'
+                echo
+                echo '====  ===  ==== =   =      ==  ====  ==  ==='
+                echo '=  =  =  = =    ==  =     =  = =    =  = =  ='
+                echo '=  =  ===  ==== = = =     ==== ==== ==== ==='
+                echo '=  =  =    =    =  ==     =  =    = =  = =  ='
+                echo '====  =    ==== =   =  =  =  = ==== =  = =  ='
+                echo
+                echo 'OpenAsar should be installed! You can check by looking for an "OpenAsar" option in your Discord settings.'
+                echo 'Not installed? Try restarting Discord, running this script again, filing a GitHub issue or contacting @aaronliu on Discord.'
+                if [[ $spaceballs == 6 ]]; then
+                echo
+                echo "If you can read this, you don't need glasses."
+                export spaceballs="If you can read this, you don't need glasses."
+                fi
+                sleep 1
+                clear
+                echo
+                echo 'Installer updated and maintained by aaronliu0130 (@aaronliu)'
+                echo
+                echo '....  ...  .... .   .      ..  ....  ..  ...'
+                echo '.  .  .  . .    ..  .     .  . .    .  . .  .'
+                echo '.  .  ...  .... . . .     .... .... .... ...'
+                echo '.  .  .    .    .  ..     .  .    . .  . .  .'
+                echo '....  .    .... .   .  .  .  . .... .  . .  .'
+                echo
+                echo 'OpenAsar should be installed! You can check by looking for an "OpenAsar" option in your Discord settings.'
+                echo 'Not installed? Try restarting Discord, running this script again, filing a GitHub issue or contacting @aaronliu on Discord.'
+                exit 0
+            fi
+        fi
+        if [[ -d "$file" ]]; then
+            echo Please enter the path to the file, not a directory.
+            read -r file
+        fi
+        if [[ ! -e "$file" ]]; then
+            echo "$file doesn't exist. Exiting..."
+            exit 2
+        fi
+    fi
+fi
+echo "Found app.asar at $file. Press enter to continue and ^C (Ctrl+C) to quit."
+read -r
+
+echo
+paskill=('Discord' 'DiscordPTB' 'DiscordCanary')
+echo "Killing ${plain[$ans]} (if found)..."
+killall "${paskill[ans]}"
+until ! killall "${paskill[ans]}" -s 0; do
+    sleep 1
+done
+
+echo Installing...
+echo
+echo '1. Backing up original app.asar(s) to app.asar(s).backup...'
+mv "$file" "$file.backup"
+if ! mv "$file" "$file.backup"; then
+    echo Modification failed, please provide the sudo password to elevate and retry.
+    if ! sudo mv "$file" "$file.backup"; then
+        echo Failed even with elevation. Please file an issue and report this. Exiting...
+        exit 77
+    fi
+    # See the other comment below.
+    if [[ -e "$(dirname "$file")/_app.asar" ]]; then
+        sudo mv "$(dirname "$file")/_app.asar" "$(dirname "$file")/_app.asar.backup"
+    fi
+    if [[ -e "$(file).orig" ]]; then
+        sudo mv "$(file).orig" "$(file).orig.backup"
+    fi
+else
+    # I don't think the following code is necessary, I think we shouldn't tamper with stuff we don't change, but the original installer does this
+    if [[ -e "$(dirname "$file")/_app.asar" ]]; then mv "$(dirname "$file")/_app.asar" "$(dirname "$file")/_app.asar.backup"; fi
+    if [[ -e "$file.orig" ]]; then mv "$file.orig" "$file.orig.backup"; fi
+fi
+
+echo '2. Downloading OpenAsar...'
+if ! curl -sLo "$file" 'https://github.com/GooseMod/OpenAsar/releases/download/latest/app.asar'; then
+    echo Downloading failed, retrying with sudo...
+    if ! sudo curl -sLo "$file" 'https://github.com/GooseMod/OpenAsar/releases/download/latest/app.asar'; then
+        echo Failed even with elevation. Please file an issue and report this. Exiting...
+        exit 77
+    fi
+    if [[ -e $(dirname "$file")/_app.asar.backup ]]; then sudo cp "$file" "$(dirname "$file")/_app.asar"; fi
+    if [[ -e "$file.orig.backup" ]]; then sudo cp "$file" "$file.orig.backup"; fi
+else
+    if [[ -e $(dirname "$file")/_app.asar.backup ]]; then cp "$file" "$(dirname "$file")/_app.asar"; fi
+    if [[ -e "$file.orig.backup" ]]; then cp "$file" "$file.orig.backup"; fi
+fi
+echo All clear!
+sleep 1
+clear
+echo
+echo 'Installer updated and maintained by aaronliu0130 (@aaronliu)'
+echo
+echo '####  ###  #### #   #      ##  ####  ##  ###'
+echo '#  #  #  # #    ##  #     #  # #    #  # #  #'
+echo '#  #  ###  #### # # #     #### #### #### ###'
+echo '#  #  #    #    #  ##     #  #    # #  # #  #'
+echo '####  #    #### #   #  #  #  # #### #  # #  #'
+echo
+echo 'OpenAsar should be installed! You can check by looking for an "OpenAsar" option in your Discord settings.'
+echo 'Not installed? Try restarting Discord, running this script again, filing a GitHub issue or contacting @aaronliu on Discord.'
